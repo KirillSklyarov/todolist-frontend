27b51e3ec5272a1adc8a87d1b7decbd5e0b975c9
Your Name <you@example.com>
2019-02-20 00:10:53 +0300

    chore: remove exclude directive

--- src/app/directives/exclude.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExcludeDirective } from './exclude.directive';
-
-describe('ExcludeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ExcludeDirective();
-    expect(directive).toBeTruthy();
-  });
-});
