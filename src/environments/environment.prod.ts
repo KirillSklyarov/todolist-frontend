f677a04d609d2d698f57f4c35e2eeb5e6ca73d1c
Your Name <you@example.com>
2019-02-19 21:33:15 +0300

    feat: env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,30 @@ import {env} from './env/env.prod';
 
 export const environment = {
   apiServer: env.apiServer || '',
-  production: true
+  production: true,
+  maxCountPerPage: 10,
+  defaultCountPerPage: 10,
+  tokenLifetime: {
+    unregistred: {
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      days: 30,
+      years: 0,
+    },
+    registred: {
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+      days: 30,
+      years: 0,
+    }
+  },
+  errors: {
+    connection: 'Connection error. Try again later',
+    input: 'Input error',
+    auth: 'Wrong login or password',
+    token: 'Wrong token',
+    server: 'Server error. Try again later',
+  }
 };
